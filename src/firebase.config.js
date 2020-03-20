dd82fccb9482811a54fbcb0c3bb65b06f49bc8f6
rakibjim <rmjimg@gmail.com>
2020-03-20 15:37:57 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDVGdlAEmVo38_koGsmAuKMwblOqLosEmE",
+    authDomain: "ema-john-simple-5e705.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-5e705.firebaseio.com",
+    projectId: "ema-john-simple-5e705",
+    storageBucket: "ema-john-simple-5e705.appspot.com",
+    messagingSenderId: "108012665607",
+    appId: "1:108012665607:web:bab00bf3f5709e87efe3a2"
+  };
+
+  export default firebaseConfig;
